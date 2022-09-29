37d8bbd6206957fb57823e8b529c895b9d992d47
unknown <bohdan655628@gmail.com>
2022-09-29 22:37:42 +0300

    Karmaniuk changes

--- calculator.cpp
+++ calculator.cpp
@@ -1,16 +1,23 @@
 #include "calculator.h"
 
+//add change
 int Calculator::Add (double a, double b)
 {
 	return a + b + 0.5;
 }
 
+//sub change
 int Calculator::Sub (double a, double b)
 {
     return Add (a, -b);
 }
 
+//mul change
 int Calculator::Mul (double a, double b)
 {
     return a * b + 0.5;
 }
+
+//Karmaniuk
+//Bohdan
+//IO-04
